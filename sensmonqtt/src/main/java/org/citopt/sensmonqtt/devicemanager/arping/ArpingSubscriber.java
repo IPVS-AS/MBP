8a1f6079eae7b6e07021b3dc99c7975b0a517424
Rafael Koch Peres <Rafael Koch Peres>
2016-06-17 15:30:36 +0200

    ArpingSubscriber and tests done. Will connect to MQTT arping topic, parse messages and put entries on its map.

--- sensmonqtt/src/main/java/org/citopt/sensmonqtt/devicemanager/arping/ArpingSubscriber.java
+++ sensmonqtt/src/main/java/org/citopt/sensmonqtt/devicemanager/arping/ArpingSubscriber.java
@@ -5,11 +5,16 @@
  */
 package org.citopt.sensmonqtt.devicemanager.arping;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -19,7 +24,41 @@ import org.json.simple.parser.ParseException;
  *
  * @author rafaelkperes
  */
-public class ArpingSubscriber {
+public class ArpingSubscriber implements MqttCallback {
+
+    private final Map<String, String> mac2Ip;
+    private MqttClient client;
+
+    private static final String ARPING_TOPIC = "arping/result";
+    private static final String ARPING_BROKER_URL = "tcp://localhost";
+    private static final String ARPING_CLIENT_ID
+            = "org.citopt.sensmonqtt.devicemanager.arping";
+
+    public ArpingSubscriber() {
+        this.mac2Ip = new HashMap<>();
+        this.client = null;
+    }
+
+    public String getIp(String macAddress) throws NoSuchElementException {
+        String ip = mac2Ip.get(macAddress);
+        if (ip != null) {
+            return ip;
+        }
+        throw new NoSuchElementException();
+    }
+
+    public void connectMqtt() throws MqttException {
+        MemoryPersistence persistence = new MemoryPersistence();
+        this.client = new MqttClient(ARPING_BROKER_URL, ARPING_CLIENT_ID,
+                persistence);
+        client.connect();
+        client.setCallback(this);
+        client.subscribe(ARPING_TOPIC);
+    }
+
+    public boolean isConnected() {
+        return this.client != null && client.isConnected();
+    }
 
     public Map<String, String> parseJson(String toParse) throws ParseException {
         Map<String, String> parsed = new HashMap<>();
@@ -41,6 +80,27 @@ public class ArpingSubscriber {
         }
 
         return parsed;
+    }
+
+    @Override
+    public void connectionLost(Throwable thrwbl) {
+    }
+
+    /**
+     * Will add any new entries received and replace old ones.
+     *
+     * @param string
+     * @param mm
+     * @throws org.json.simple.parser.ParseException
+     */
+    @Override
+    public void messageArrived(String string, MqttMessage mm) throws ParseException {
+        Map<String, String> newEntries = parseJson(mm.toString());
+        this.mac2Ip.putAll(newEntries);
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken imdt) {
     }
 
 }
--- sensmonqtt/src/test/java/org/citopt/sensmonqtt/devicemanager/arping/ArpingSubscriberTest.java
+++ sensmonqtt/src/test/java/org/citopt/sensmonqtt/devicemanager/arping/ArpingSubscriberTest.java
@@ -7,6 +7,9 @@ package org.citopt.sensmonqtt.devicemanager.arping;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.simple.parser.ParseException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,37 +22,38 @@ import static org.junit.Assert.*;
  * @author rafaelkperes
  */
 public class ArpingSubscriberTest {
-    
+
     private static String parseable;
-    
+
     public ArpingSubscriberTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
         parseable = "{\"iptomac\": [[\"100.70.2.142\", \"b0:a8:6e:9a:8f:13\"], [\"100.70.2.140\", \"b8:27:eb:91:aa:c3\"]]}";
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     /**
      * Test of parseJson method, of class ArpingSubscriber.
+     *
      * @throws java.lang.Exception
      */
     @Test
     public void testParseJson() throws Exception {
         System.out.println("parseJson");
-        String toParse = parseable;
+        String toParse = "{\"iptomac\": [[\"100.70.2.142\", \"b0:a8:6e:9a:8f:13\"], [\"100.70.2.140\", \"b8:27:eb:91:aa:c3\"]]}";
         ArpingSubscriber instance = new ArpingSubscriber();
         Map<String, String> expResult = new HashMap<>();
         expResult.put("b0:a8:6e:9a:8f:13", "100.70.2.142");
@@ -57,5 +61,108 @@ public class ArpingSubscriberTest {
         Map<String, String> result = instance.parseJson(toParse);
         assertEquals(expResult, result);
     }
-    
+
+    /**
+     * Test of connectMqtt method, of class ArpingSubscriber.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testConnectMqtt() throws Exception {
+        System.out.println("connectMqtt");
+        ArpingSubscriber instance = new ArpingSubscriber();
+        instance.connectMqtt();
+        assertTrue(instance.isConnected());
+
+        String toParse = "derp";
+        MqttMessage mm = new MqttMessage(toParse.getBytes());
+        try {
+            instance.messageArrived("", mm);
+            fail("Should throw Exception");
+        } catch (ParseException ex) {
+            // should stay connected after ParseException
+            assertTrue(instance.isConnected());
+        }
+    }
+
+    /**
+     * Test of isConnected method, of class ArpingSubscriber.
+     */
+    @Test
+    public void testIsConnected() {
+        System.out.println("isConnected");
+        ArpingSubscriber instance = new ArpingSubscriber();
+        boolean expResult = false;
+        boolean result = instance.isConnected();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getIp method, of class ArpingSubscriber.
+     *
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testGetIp() throws Exception {
+        System.out.println("getIp");
+        String macAddress = "b0:a8:6e:9a:8f:13";
+        ArpingSubscriber instance = new ArpingSubscriber();
+
+        String toParse = "{\"iptomac\": [[\"100.70.2.142\", \"b0:a8:6e:9a:8f:13\"], [\"100.70.2.140\", \"b8:27:eb:91:aa:c3\"]]}";
+        instance.messageArrived("topic", new MqttMessage(toParse.getBytes()));
+
+        String expResult = "100.70.2.142";
+        String result = instance.getIp(macAddress);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of connectionLost method, of class ArpingSubscriber.
+     */
+    @Test
+    public void testConnectionLost() {
+        System.out.println("connectionLost");
+        Throwable thrwbl = null;
+        ArpingSubscriber instance = new ArpingSubscriber();
+        instance.connectionLost(thrwbl);
+    }
+
+    /**
+     * Test of messageArrived method, of class ArpingSubscriber.
+     */
+    @Test
+    public void testMessageArrived() {
+        System.out.println("messageArrived");
+        String string = "";
+        String toParse = "derp";
+        MqttMessage mm = new MqttMessage(toParse.getBytes());
+        ArpingSubscriber instance = new ArpingSubscriber();
+        try {
+            instance.messageArrived(string, mm);
+            fail("Should throw Exception");
+        } catch (ParseException ex) {
+        }
+
+        string = "";
+        toParse = "{\"iptomac\": [[\"100.70.2.142\", \"b0:a8:6e:9a:8f:13\"], [\"100.70.2.140\", \"b8:27:eb:91:aa:c3\"]]}";
+        mm = new MqttMessage(toParse.getBytes());
+        instance = new ArpingSubscriber();
+        try {
+            instance.messageArrived(string, mm);
+        } catch (ParseException ex) {
+            fail(ex.toString());
+        }
+    }
+
+    /**
+     * Test of deliveryComplete method, of class ArpingSubscriber.
+     */
+    @Test
+    public void testDeliveryComplete() {
+        System.out.println("deliveryComplete");
+        IMqttDeliveryToken imdt = null;
+        ArpingSubscriber instance = new ArpingSubscriber();
+        instance.deliveryComplete(imdt);
+    }
+
 }
