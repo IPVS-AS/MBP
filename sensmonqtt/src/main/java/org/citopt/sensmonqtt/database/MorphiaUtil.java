dd8846edd824bb4bc920ac2999eb7639a673b99a
Rafael Koch Peres <Rafael Koch Peres>
2016-07-20 00:58:05 +0200

    Added flame to sensor.

---
+++ sensmonqtt/src/main/java/org/citopt/sensmonqtt/database/MorphiaUtil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.citopt.sensmonqtt.database;
+
+import com.mongodb.MongoClient;
+import java.net.UnknownHostException;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+
+/**
+ *
+ * @author rafaelkperes
+ */
+public class MorphiaUtil {
+
+    public static Datastore getDatastore() throws UnknownHostException {
+        MongoClient mongoClient = new MongoClient();
+        final Morphia morphia = new Morphia();
+        final Datastore datastore = morphia.createDatastore(mongoClient, "sensor");
+        datastore.ensureIndexes();
+        
+        morphia.mapPackage("org.citopt.sensmonqtt.device");
+        morphia.mapPackage("org.citopt.sensmonqtt.user");
+        
+        return datastore;
+    }
+    
+}
