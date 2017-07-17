9cde03a361b59da846bc8816b61e7f0801d7671b
ana-silva <anacristina.francodasilva@gmail.com>
2017-07-17 11:16:37 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 !/bin/sh
+sudo sh -c "echo '127.0.0.1' $(hostname) >> /etc/hosts";
 sudo apt-get -qy update;
-sudo apt-get -qy upgrade;
-#Installing Java8
+#sudo apt-get -qy upgrade;
+
+#Installing Java8, pip, python mqtt client
 sudo apt-get install -qy openjdk-8-jdk;
+sudo apt-get install -qy python-pip;
+sudo pip install paho-mqtt;
+sudo pip install pymongo;
 
 # Install Mosquitto Broker
 sudo apt-get install -y mosquitto;
@@ -13,7 +18,9 @@ sudo /etc/init.d/mongodb start;
 sudo /etc/init.d/mongodb status;
 
 # Install Tomcat 9
-sudo apt-get install -y tomcat8 git maven;
+sudo apt-get install -y tomcat8;
+sudo apt-get install -y git;
+sudo apt-get install -y maven;
 
 # checkout repository
 sudo git clone https://github.com/ana-silva/connde.git
--- java-projects/restful-connde/src/main/java/org/citopt/connde/RootConfiguration.java
+++ java-projects/restful-connde/src/main/java/org/citopt/connde/RootConfiguration.java
@@ -4,6 +4,7 @@ import com.mongodb.Mongo;
 import java.net.UnknownHostException;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -16,19 +17,22 @@ import org.springframework.context.annotation.Import;
 @Import({MongoConfiguration.class})
 public class RootConfiguration {
 
-//    @Bean(name = "mqtt")
-//    public MqttClient mqttClient()  {
-//        System.out.println("load MqttClient");
-//        // CHANGE TO DYNAMIC IP
-//        MqttClient mqttClient = null;
-////		try {
-////			mqttClient = new MqttClient("tcp://localhost:1883", "root-server");
-////		} catch (MqttException e) {
-////			// TODO Auto-generated catch block
-////			e.printStackTrace();
-////		}
-//        return mqttClient;
-//    }
+    @Bean(name = "mqtt")
+    public MqttClient mqttClient()  {
+        System.out.println("load MqttClient");
+        // CHANGE TO DYNAMIC IP
+        MqttClient mqttClient = null;
+        MemoryPersistence persistence = new MemoryPersistence();
+
+		try {
+			mqttClient = new MqttClient("tcp://localhost:1883", "root-server", persistence);
+		} catch (MqttException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("reason " + e.getReasonCode());
+		}
+        return mqttClient;
+    }
     
     @Bean(name = "mongo")
     public Mongo mongo() throws UnknownHostException {
