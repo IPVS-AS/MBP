7e78a4610b1d456a6bd30d1484b3b903e16954e9
Jan Schneider <st117301@stud.uni-stuttgart.de>
2018-11-16 23:18:08 +0100

    Replaced deprecated methods/classes and upgraded mongo driver Resolves #10 and #11

--- src/main/java/org/citopt/connde/RestConfiguration.java
+++ src/main/java/org/citopt/connde/RestConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.Resource;
@@ -18,16 +19,15 @@ import org.springframework.hateoas.ResourceProcessor;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
 /**
- *
  * @author rafaelkperes
  */
 @Configuration
-public class RestConfiguration extends RepositoryRestMvcConfiguration {
+public class RestConfiguration extends RepositoryRestConfigurerAdapter {
 
     public static final String BASE_PATH = "/api";
 
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         System.out.println("load RepositoryRestMvcConfiguration");
         super.configureRepositoryRestConfiguration(config);
         config.setBasePath(BASE_PATH);
@@ -36,7 +36,7 @@ public class RestConfiguration extends RepositoryRestMvcConfiguration {
     }
 
     @Override
-    protected void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener v) {
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener v) {
         v.addValidator("beforeSave", new SensorValidator());
         v.addValidator("beforeCreate", new SensorValidator());
 
@@ -46,35 +46,26 @@ public class RestConfiguration extends RepositoryRestMvcConfiguration {
 
     @Bean
     public ResourceProcessor<Resource<Sensor>> sensorProcessor() {
-
-        return new ResourceProcessor<Resource<Sensor>>() {
-
-            @Override
-            public Resource<Sensor> process(Resource<Sensor> resource) {
-                String id = resource.getContent().getId();
-                Link link = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.
-                        methodOn(RestApiController.class).deploySensor(id))
-                        .withRel("deploy");
-                resource.add(link);
-                return resource;
-            }
+        return resource -> {
+            String id = resource.getContent().getId();
+            Link link = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.
+                    methodOn(RestApiController.class).deploySensor(id))
+                    .withRel("deploy");
+            resource.add(link);
+            return resource;
         };
     }
 
     @Bean
     public ResourceProcessor<Resource<Actuator>> actuatorProcessor() {
 
-        return new ResourceProcessor<Resource<Actuator>>() {
-
-            @Override
-            public Resource<Actuator> process(Resource<Actuator> resource) {
-                String id = resource.getContent().getId();
-                Link link = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.
-                        methodOn(RestApiController.class).deployActuator(id))
-                        .withRel("deploy");
-                resource.add(link);
-                return resource;
-            }
+        return resource -> {
+            String id = resource.getContent().getId();
+            Link link = ControllerLinkBuilder.linkTo(ControllerLinkBuilder.
+                    methodOn(RestApiController.class).deployActuator(id))
+                    .withRel("deploy");
+            resource.add(link);
+            return resource;
         };
     }
 }
