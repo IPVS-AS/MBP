fabc4da856a611f441f0d51e0346c78a39a6d6b4
Jan Schneider <st117301@stud.uni-stuttgart.de>
2018-12-16 17:28:11 +0100

    Improved IP address resolver for ubuntu devices

--- src/main/java/org/citopt/connde/service/NetworkService.java
+++ src/main/java/org/citopt/connde/service/NetworkService.java
@@ -2,8 +2,7 @@ package org.citopt.connde.service;
 
 import org.springframework.stereotype.Component;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.*;
 
 /**
  * Network service that provides basic network-related features.
@@ -13,12 +12,35 @@ import java.net.UnknownHostException;
 @Component
 public class NetworkService {
     /**
-     * Retrieves the own IP address of the device this application is currently running on.
+     * Retrieves the own local IP address of the device this application is currently running on. Works even with
+     * multiple available network interfaces.     *
      *
      * @return The IP address of the underlying device
      * @throws UnknownHostException In case of an unexpected network issue
      */
-    public String getOwnIPAddress() throws UnknownHostException {
-        return InetAddress.getLocalHost().getHostAddress();
+    public String getOwnIPAddress() throws UnknownHostException, SocketException {
+        //Try to resolve IP address in an easy way
+        String ipAddress = Inet4Address.getLocalHost().getHostAddress();
+
+        //Check if the resolved address may be correct
+        if ((!ipAddress.equals("127.0.0.1")) && (!ipAddress.equals("localhost"))) {
+            return ipAddress;
+        }
+
+        /*
+        Resolved IP address is useless, thus try another way
+        */
+
+        //Create UDP socket
+        DatagramSocket socket = new DatagramSocket();
+
+        //Pretend connection to an arbitrary address (does not need to be reachable)
+        socket.connect(InetAddress.getByName("199.199.199.199"), 12321);
+
+        //Resolve IP address
+        ipAddress = socket.getLocalAddress().getHostAddress();
+        socket.close();
+
+        return ipAddress;
     }
 }
