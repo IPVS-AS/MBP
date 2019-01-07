2d5533c46973d82b71d71fe96d73c5e6c66359c5
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-01-07 23:40:27 +0100

    Fixed two other related bugs in SensorDetailsController; now no javascript error messages should occur anymore.

--- src/main/resources/static/js/controllers/sensors/SensorDetailsController.js
+++ src/main/resources/static/js/controllers/sensors/SensorDetailsController.js
@@ -178,7 +178,7 @@ app.controller('SensorDetailsController',
                                         	
                                         	var finalValues = new Array();
                                         	var i = data._embedded.valueLogs.length - 1;
-                                        	for (i; i != 0; i--) {
+                                        	for (i; i > 0; i--) {
                                         		var values = new Array();
                                         		var value = data._embedded.valueLogs[i].value * 1;
                                         		
@@ -187,8 +187,6 @@ app.controller('SensorDetailsController',
                                         		
                                         		var parsedDate = new Date(date);
                                         		values.push(parsedDate.toString(), value);
-                                        		
-                                        		//values.push(data._embedded.valueLogs.length - i, value);
                                         		
                                         		finalValues.push(values);
                                         	}
@@ -223,17 +221,18 @@ app.controller('SensorDetailsController',
 
                                                                             CrudService.searchPage('valueLogs', query, params).then(
                                                                                 function (data) {
-                                                                                	var value = 0.0;
-                                                                                	value = data._embedded.valueLogs[0].value * 1.0;
-                                                                                	
-                                                                            		var date = data._embedded.valueLogs[i].date;
+                                                                                    //Sanity check
+                                                                                    if(data._embedded.valueLogs.length < 1){
+                                                                                        return;
+                                                                                    }
+
+                                                                                	var value = data._embedded.valueLogs[0].value * 1.0;
+                                                                            		var date = data._embedded.valueLogs[0].date;
                                                                             		date = date.replace(/\s/g, "T");
                                                                             		
                                                                             		var parsedDate = new Date(date);
 
-                                                                                	if (data._embedded.valueLogs.length > 0) {
-                                                                                		series.addPoint([parsedDate.toString(), value], true, true);
-                                                                                	}
+                                                                                	series.addPoint([parsedDate.toString(), value], true, true);
                                                                                 }
                                                                             );
                                                                         }, 500);
