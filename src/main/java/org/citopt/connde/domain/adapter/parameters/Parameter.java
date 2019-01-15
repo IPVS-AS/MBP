71731eef74587099b04ad0e143c066fd9fb49f36
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-01-15 23:38:23 +0100

    Finished code documentation

--- src/main/java/org/citopt/connde/domain/adapter/parameters/Parameter.java
+++ src/main/java/org/citopt/connde/domain/adapter/parameters/Parameter.java
@@ -1,64 +1,126 @@
 package org.citopt.connde.domain.adapter.parameters;
 
-
+/**
+ * Objects of this class represent deployment parameters that can be added to adapters. If a sensor is
+ * supposed to be deployed that uses such an adapter, the values for the parameters need to be provided
+ * as parameter instances by the user.
+ */
 public class Parameter {
     private String name;
     private ParameterTypes type;
     private boolean mandatory;
 
-    public Parameter(){
+    /**
+     * Creates a new empty deployment parameter that may be added to an adapter.
+     */
+    public Parameter() {
 
     }
 
+    /**
+     * Creates a new deployment parameter with a given name and parameter type. In addition, it can
+     * be specified whether the parameter is mandatory for the deployment.
+     *
+     * @param name      The name of the parameter
+     * @param type      The type of the parameter
+     * @param mandatory True, if the parameter is mandatory; false otherwise
+     */
+    public Parameter(String name, ParameterTypes type, boolean mandatory) {
+        this.name = name;
+        this.type = type;
+        this.mandatory = mandatory;
+    }
+
+    /**
+     * Returns the name of the parameter.
+     *
+     * @return The name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the parameter.
+     *
+     * @param name The name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the type of the parameter.
+     *
+     * @return The type
+     */
     public ParameterTypes getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the parameter.
+     *
+     * @param type The type
+     */
     public void setType(ParameterTypes type) {
         this.type = type;
     }
 
+    /**
+     * Returns whether the parameter is mandatory.
+     *
+     * @return True, if the parameter is mandatory; false otherwise
+     */
     public boolean isMandatory() {
         return mandatory;
     }
 
+    /**
+     * Determines whether the parameter is mandatory.
+     *
+     * @param mandatory True, if the parameter is mandatory; false otherwise
+     */
     public void setMandatory(boolean mandatory) {
         this.mandatory = mandatory;
     }
 
-    public boolean isInstanceValid(ParameterInstance instance){
+    /**
+     * Verifies if a given parameter instance is a valid instance for this parameter. In order to
+     * be valid, the instance name and type must correspond to the name and type of the parameter.
+     *
+     * @param instance The parameter instance to check
+     * @return True, if the parameter instance is a valid instance; false otherwise
+     */
+    public boolean isInstanceValid(ParameterInstance instance) {
         //Check name
-        if(!instance.getName().equals(this.name)){
+        if (!instance.getName().equals(this.name)) {
             return false;
         }
 
         //Check data type
         Object value = instance.getValue();
-        switch(this.type){
+        switch (this.type) {
             case BOOLEAN:
-                if(!(value instanceof Boolean)){
+                //Value must be of type boolean
+                if (!(value instanceof Boolean)) {
                     return false;
                 }
                 break;
             case NUMBER:
-                if(!((value instanceof Double) || (value instanceof Integer))){
+                //Value must be either of type double or integer
+                if (!((value instanceof Double) || (value instanceof Integer))) {
                     return false;
                 }
                 break;
             case TEXT:
-                if(!(value instanceof String)){
+                //Value must be of type string
+                if (!(value instanceof String)) {
                     return false;
                 }
                 String stringValue = (String) value;
-                if(this.mandatory && stringValue.isEmpty()){
+                //Value most not be empty except if the parameter is not mandatory
+                if (this.mandatory && stringValue.isEmpty()) {
                     return false;
                 }
                 break;
--- src/main/java/org/citopt/connde/domain/adapter/parameters/ParameterInstance.java
+++ src/main/java/org/citopt/connde/domain/adapter/parameters/ParameterInstance.java
@@ -1,25 +1,62 @@
 package org.citopt.connde.domain.adapter.parameters;
 
-
+/**
+ * Objects of this class represent instantiated deployment parameters for which an actual value was provided.
+ */
 public class ParameterInstance {
+    //Name and value of the parameter
     private String name;
     private Object value;
 
+    /**
+     * Creates a new empty instance for a parameter without name and value.
+     */
     public ParameterInstance() {
     }
 
+    /**
+     * Creates a new instance for a parameter with a given name and value.
+     *
+     * @param name  The name of the parameter to which the instance belongs
+     * @param value The parameter value
+     */
+    public ParameterInstance(String name, Object value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    /**
+     * Returns the name of the parameter to which the instance belongs.
+     *
+     * @return The name of the parameter
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the parameter to which the instance belongs.
+     *
+     * @param name The name of the parameter
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the value of this parameter instance.
+     *
+     * @return The parameter value
+     */
     public Object getValue() {
         return value;
     }
 
+    /**
+     * Sets the value of this parameter instance.
+     *
+     * @param value The parameter value
+     */
     public void setValue(Object value) {
         this.value = value;
     }
