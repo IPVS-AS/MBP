56e81188309a894cc454a021c3ee2581033cd0af
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-04-29 12:45:31 +0200

    Added comments and documentation

--- src/main/java/org/citopt/connde/service/cep/core/CEPEngine.java
+++ src/main/java/org/citopt/connde/service/cep/core/CEPEngine.java
@@ -1,7 +1,7 @@
 package org.citopt.connde.service.cep.core;
 
 import com.espertech.esper.client.*;
-import org.citopt.connde.service.cep.core.events.CEPDataType;
+import org.citopt.connde.service.cep.core.events.CEPPrimitiveDataTypes;
 import org.citopt.connde.service.cep.core.events.CEPEvent;
 import org.citopt.connde.service.cep.core.events.CEPEventType;
 import org.citopt.connde.service.cep.core.queries.CEPQuery;
@@ -9,48 +9,79 @@ import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+/**
+ * This component works as a wrapper for the CEP engine Esper (http://esper.espertech.com) and
+ * provides basic functionality for working with this engine independently from the MBP.
+ */
 @Component
 public class CEPEngine {
-
+    //Internal fields
     private EPServiceProvider cepService;
     private EPAdministrator cepAdmin;
     private EPRuntime cepRuntime;
 
+    //Stores the event types that have been registered at the engine
     private Set<CEPEventType> registeredEventTypes;
 
+    /**
+     * Creates the component by initializing Esper and the corresponding internal fields.
+     */
     private CEPEngine() {
+        //Get and initialize CEP service
         cepService = EPServiceProviderManager.getDefaultProvider();
         cepService.initialize();
 
+        //Get admin and runtime objects
         cepAdmin = cepService.getEPAdministrator();
         cepRuntime = cepService.getEPRuntime();
 
+        //Create empty set of registered event types
         registeredEventTypes = new HashSet<>();
     }
 
+    /**
+     * Creates and registers a new CEP query from a given name and a query string in EPL (Event Processing Language).
+     * The registered query is then returned as a CEPQuery object which provides further functionality, such as
+     * means for subscription.
+     *
+     * @param name        The name of the query to create
+     * @param queryString The query string of the query described in Esper's Event Processing Language
+     * @return The CEPQuery object representing the query
+     */
     public CEPQuery createQuery(String name, String queryString) {
+        //Sanity checks
         if ((name == null) || (name.isEmpty())) {
             throw new IllegalArgumentException("Name must not be null or empty.");
         } else if ((queryString == null) || (queryString.isEmpty())) {
             throw new IllegalArgumentException("Query string must not be null or empty.");
         }
 
+        //Check for duplicate query names
         List<String> statementNames = getAllQueryNames();
         if (statementNames.contains(name)) {
             throw new IllegalArgumentException("A query with this name is already registered.");
         }
 
+        //Create statement with name and query string
         EPStatement statement = cepAdmin.createEPL(queryString, name);
+
+        //Create query object from statement and return
         return new CEPQuery(statement);
     }
 
+    /**
+     * Returns a CEPQuery object for a certain query given by its name.
+     *
+     * @param name The name of the query for which the CEPQuery object is supposed to be returne
+     * @return A dedicated CEPQuery object representing the query
+     */
     public CEPQuery getQueryByName(String name) {
         //Sanity check
         if ((name == null) || (name.isEmpty())) {
             throw new IllegalArgumentException("Name must not be null or empty.");
         }
 
-        //Retrieve statement with given name
+        //Retrieve statement by name
         EPStatement statement = cepAdmin.getStatement(name);
 
         //Sanity check
@@ -58,18 +89,31 @@ public class CEPEngine {
             throw new IllegalArgumentException("A query with name \"" + name + "\" is not registered.");
         }
 
+        //Create query object from statement and return
         return new CEPQuery(statement);
     }
 
+    /**
+     * Registers a new event type to the CEP engine. After that, events of the new event type might be
+     * sent to the engine or used in queries.
+     *
+     * @param eventType The event type to register
+     */
     public void registerEventType(CEPEventType eventType) {
+        //Sanity checks
         if (eventType == null) {
             throw new IllegalArgumentException("Event type object must not be null.");
         } else if (registeredEventTypes.contains(eventType)) {
             throw new IllegalArgumentException("This event type has already been registered.");
         }
 
+        /*
+        Build up a query string containing the event name and its typed fields for registering
+        the event type at the Esper engine.
+         */
+
         //Get fields of event object
-        Map<String, CEPDataType> eventFields = eventType.getFields();
+        Map<String, CEPPrimitiveDataTypes> eventFields = eventType.getFields();
 
         //Build query
         StringBuilder queryBuilder = new StringBuilder("Create schema");
@@ -78,21 +122,24 @@ public class CEPEngine {
         queryBuilder.append("(");
 
         //Iterate over all event fields
-        Set<String> fieldNames = eventFields.keySet();
-        Iterator<String> fieldNameIterator = fieldNames.iterator();
+        Iterator<String> fieldNameIterator = eventFields.keySet().iterator();
         while (fieldNameIterator.hasNext()) {
+            //Get current field name and type
             String fieldName = fieldNameIterator.next();
-            CEPDataType fieldType = eventFields.get(fieldName);
+            CEPPrimitiveDataTypes fieldType = eventFields.get(fieldName);
 
+            //Append field name and its type to the query
             queryBuilder.append(fieldName);
             queryBuilder.append(" ");
-            queryBuilder.append(fieldType.getTypeName());
+            queryBuilder.append(fieldType.getName());
 
+            //Only add a comma if there are further fields to process
             if (fieldNameIterator.hasNext()) {
                 queryBuilder.append(", ");
             }
         }
 
+        //Close query
         queryBuilder.append(")");
 
         //Create statement for query
@@ -102,10 +149,17 @@ public class CEPEngine {
         registeredEventTypes.add(eventType);
     }
 
+    /**
+     * Sends a given event to the CEP engine such that it can be processed by Esper. The type of the event
+     * that is supposed to be sent to the engine needs to be registered before.
+     *
+     * @param event The event to send
+     */
     public void sendEvent(CEPEvent event) {
-        //Try to find matching event type in set of registered events
+        //Iterate over all registered event types and Try to find matching event type
         CEPEventType matchingEventType = null;
         for (CEPEventType eventType : registeredEventTypes) {
+            //Check if current event type matches the given event
             if (eventType.isValidInstance(event)) {
                 matchingEventType = eventType;
                 break;
@@ -117,12 +171,20 @@ public class CEPEngine {
             throw new IllegalArgumentException("No event type has been registered for this event.");
         }
 
-        //Send event since it is valid
+        //Send valid event to Esper
         cepRuntime.sendEvent(event.getFieldValues(), event.getEventTypeName());
     }
 
+    /**
+     * Returns a list of all names of queries that are currently registered at the engine.
+     *
+     * @return The list of names
+     */
     public List<String> getAllQueryNames() {
+        //Get names
         String[] names = cepAdmin.getStatementNames();
+
+        //Convert array to list
         return Arrays.asList(names);
     }
 }
--- src/main/java/org/citopt/connde/service/cep/core/events/CEPEventType.java
+++ src/main/java/org/citopt/connde/service/cep/core/events/CEPEventType.java
@@ -4,17 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Objects of this class represent definitions of event types that might be registered at the CEP engine.
+ * A event type definition consists out of a name and a map of typed fields that need to be available
+ * in event instances.
+ */
 public class CEPEventType {
+    //Name of the event type and its instances
     private String name;
-    private Map<String, CEPDataType> fields;
+    //Map (field name --> data type) of fields that are part of instances of this event type
+    private Map<String, CEPPrimitiveDataTypes> fields;
 
+    /**
+     * Creates a new event type object with a given name.
+     *
+     * @param name The name of the event type
+     */
     public CEPEventType(String name) {
-        this.name = name;
+        //Set name
+        setName(name);
+
+        //Set empty map of fields
         this.fields = new HashMap<>();
     }
 
-    public void addField(String fieldName, CEPDataType dataType) {
-        //Sanity check
+    /**
+     * Adds a field to the event type, given by a desired name and data type. The field name
+     * must be unique for an event type.
+     *
+     * @param fieldName The name of the field
+     * @param dataType  The data type of which values of the field should be
+     */
+    public void addField(String fieldName, CEPPrimitiveDataTypes dataType) {
+        //Sanity checks
         if ((fieldName == null) || (fieldName.isEmpty())) {
             throw new IllegalArgumentException("Field name must not be null or empty.");
         } else if (fields.containsKey(fieldName)) {
@@ -23,41 +45,48 @@ public class CEPEventType {
             throw new IllegalArgumentException("Data type must not be null.");
         }
 
+        //Add name and data type to field map
         fields.put(fieldName, dataType);
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public Map<String, CEPDataType> getFields() {
-        return fields;
-    }
-
-    public boolean isValidInstance(CEPEvent cepEvent) {
+    /**
+     * Checks whether a given event object is a valid instance of this event type. In order to be
+     * a valid instance of this event type, the event object needs to have the same name and to provide
+     * type-compatible values for at least the same fields that are defined in the event type. However,
+     * it is allowed to add more fields to the event object than defined in its corresponding event type.
+     *
+     * @param event The event object to check
+     * @return True, if the event object is a valid instance of this event type; false otherwise
+     */
+    public boolean isValidInstance(CEPEvent event) {
         //Sanity check
-        if (cepEvent == null) {
+        if (event == null) {
             throw new IllegalArgumentException("Event object must not be null.");
         }
 
-        //Compare event names
-        if (!cepEvent.getEventTypeName().equals(name)) {
+        //Event names must be equal
+        if (!event.getEventTypeName().equals(name)) {
             return false;
         }
 
         //Get field values of the provided event
-        Map<String, Object> fieldValues = cepEvent.getFieldValues();
+        Map<String, Object> fieldValues = event.getFieldValues();
 
         //Iterate over all defined fields of this event type
         Set<String> fieldNames = fields.keySet();
         for (String fieldName : fieldNames) {
+            //Event object must contain at least fields of the same name as those defined in the event type
             if (!fieldValues.containsKey(fieldName)) {
                 return false;
             }
 
+            //Get value of the field
             Object fieldValue = fieldValues.get(fieldName);
+
+            //Get reference class of the field as defined in the corresponding event type
             Class referenceClass = fields.get(fieldName).getReferenceClass();
 
+            //The value that is set in the event object must match the type as defined in the event type
             if (!referenceClass.isInstance(fieldValue)) {
                 return false;
             }
@@ -65,5 +94,36 @@ public class CEPEventType {
 
         //in all other cases, the event is a valid instance
         return true;
+    }
+
+    /**
+     * Returns the name of the event type.
+     *
+     * @return The name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the event type.
+     *
+     * @param name The name to set
+     */
+    public void setName(String name) {
+        //Sanity check
+        if ((name == null) || (name.isEmpty())) {
+            throw new IllegalArgumentException("Name must not be null or empty.");
+        }
+        this.name = name;
+    }
+
+    /**
+     * Returns a map (field name --> data type) of fields that need to be part of instances of this event type.
+     *
+     * @return The map of fields
+     */
+    public Map<String, CEPPrimitiveDataTypes> getFields() {
+        return fields;
     }
 }
