4e86d7a02be1fdfab08035985e88150dedc4b5a6
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-06-09 23:25:21 +0200

    Continued, looks good so far

--- src/main/resources/static/js/directives/triggers/EventPatternCreator.js
+++ src/main/resources/static/js/directives/triggers/EventPatternCreator.js
@@ -7,6 +7,40 @@
  */
 app.directive('eventPatternCreator', ['$interval', function ($interval) {
 
+    function init(scope, element, attrs) {
+        function handleCardDrop(event, ui) {
+            console.log(ui);
+            //var slotNumber = $(this).data( 'number' );
+            //var cardNumber = ui.draggable.data( 'number' );
+
+            //$(this).before(ui.draggable.clone())
+        }
+
+        //Add components to component container
+        for (var i = 0; i < scope.componentList.length; i++) {
+            var component = scope.componentList[i];
+
+            $('<div class="component"><i class="material-icons">settings_remote</i><span>' + component.name + '</span></div>')
+                .appendTo('#sensor-container .content')
+                .draggable({
+                    containment: '.pattern-creator',
+                    connectToSortable: ".pattern-creator .pattern-container",
+                    stack: '.pattern-creator .components-container .component',
+                    cursor: 'move',
+                    helper: "clone",
+                    revert: true,
+                    snap: "invalid",
+                    snapMode: "inner"
+                });
+        }
+
+        $(".pattern-creator .pattern-container").sortable({
+            //forcePlaceholderSize: false,
+            items: '> .component',
+            placeholder: 'component-placeholder'
+        });
+    }
+
     /**
      * Linking function, glue code.
      *
@@ -15,7 +49,9 @@ app.directive('eventPatternCreator', ['$interval', function ($interval) {
      * @param attrs Attributes of the directive
      */
     var link = function (scope, element, attrs) {
-
+        $(document).ready(function () {
+            init(scope, element, attrs);
+        });
     };
 
     //Configure and expose the directive
@@ -23,23 +59,18 @@ app.directive('eventPatternCreator', ['$interval', function ($interval) {
         restrict: 'E', //Elements only
         template:
             '<div class="pattern-creator">' +
-                '<div class="pattern-container">' +
-                    '<div class="event"></div>' +
-                    '<div class="event"></div>' +
-                    '<div class="event"></div>' +
-                '</div>' +
-                '<div class="components-container">' +
-                    '<div class="title"><span>Sensors</span></div>'+
-                    '<div class="content">' +
-                        '<div class="component"><i class="material-icons">settings_remote</i><span>TempSensor</span></div>' +
-                        '<div class="component"><i class="material-icons">settings_remote</i><span>AccSensor</span></div>' +
-                    '</div>' +
-                '</div>'+
+            '<div class="pattern-container">' +
+            '</div>' +
+            '<div id="sensor-container" class="components-container">' +
+            '<div class="title"><span>Sensors</span></div>' +
+            '<div class="content">' +
+            '</div>' +
+            '</div>' +
             '</div>'
         ,
         link: link,
         scope: {
-            componentList: '@componentList'
+            componentList: '=componentList'
         }
     };
 }]);
