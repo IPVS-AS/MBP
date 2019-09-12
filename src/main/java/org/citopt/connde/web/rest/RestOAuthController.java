a8a44e30b36ea1b046be3291bc281b5f9698f1be
Marvin Wohlfarth <st112462@stud.uni-stuttgart.de>
2019-09-12 12:12:54 +0200

    Adjust authorization header conversion

--- src/main/java/org/citopt/connde/web/rest/RestOAuthController.java
+++ src/main/java/org/citopt/connde/web/rest/RestOAuthController.java
@@ -38,9 +38,9 @@ public class RestOAuthController {
 
 	@RequestMapping(value = "/checkOauthTokenUser", method = RequestMethod.POST)
 	public HttpStatus checkOauthTokenUser(@RequestHeader("authorization") String authorizationHeader) {
-		LOGGER.log(Level.INFO, "############################### Authorization header looks like: " + authorizationHeader);
+		LOGGER.log(Level.INFO, "############################### Authorization header : " + authorizationHeader);
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<Json> response = restTemplate.getForEntity("http://192.168.209.207:8080/MBP/oauth/check_token?token=" + getBearerTokenFromAuthHeader(authorizationHeader), Json.class);
+		ResponseEntity<Json> response = restTemplate.getForEntity("http://192.168.209.207:8080/MBP/oauth/check_token?token=" + authorizationHeader, Json.class);
 		if (response.getStatusCode().equals(HttpStatus.OK)) {
 			LOGGER.log(Level.INFO, "CHECK OAUTH TOKEN FOR USER RETURNED OK ################");
 			return HttpStatus.OK;
