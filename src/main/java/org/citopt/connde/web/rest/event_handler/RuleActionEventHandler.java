b706077da23a093cddfda8d087307c9cf2bdbec3
Jan Schneider <st117301@stud.uni-stuttgart.de>
2019-10-17 18:03:46 +0200

    Fixes the application crash that could occur in case a rule action was deleted that is part of an enabled rule.

--- src/main/java/org/citopt/connde/web/rest/event_handler/RuleActionEventHandler.java
+++ src/main/java/org/citopt/connde/web/rest/event_handler/RuleActionEventHandler.java
@@ -3,13 +3,11 @@ package org.citopt.connde.web.rest.event_handler;
 import org.citopt.connde.domain.rules.Rule;
 import org.citopt.connde.domain.rules.RuleAction;
 import org.citopt.connde.repository.RuleRepository;
+import org.citopt.connde.service.rules.RuleEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Event handler for operations that are performed on rule actions.
@@ -21,6 +19,9 @@ public class RuleActionEventHandler {
     @Autowired
     private RuleRepository ruleRepository;
 
+    @Autowired
+    private RuleEngine ruleEngine;
+
     /**
      * Called, when a rule action is supposed to be deleted. This method then takes care of deleting
      * the rules as well that make use of this rule action.
@@ -29,22 +30,21 @@ public class RuleActionEventHandler {
      */
     @HandleBeforeDelete
     public void beforeRuleActionDelete(RuleAction ruleAction) {
-        //Stores all affected rules
-        List<Rule> affectedRules = new ArrayList<>();
-
         //Get rules that are affected by this action
         for (Rule rule : ruleRepository.findAll()) {
             //Iterate over all actions of this rule
             for (RuleAction ruleActionCheck : rule.getActions()) {
                 //Compare current rule action with the affected one
                 if (ruleActionCheck.equals(ruleAction)) {
-                    affectedRules.add(rule);
+                    //Disable rule
+                    ruleEngine.disableRule(rule);
+
+                    //Delete rule
+                    ruleRepository.delete(rule);
+
                     break;
                 }
             }
         }
-
-        //Delete all affected rules
-        ruleRepository.delete(affectedRules);
     }
 }
