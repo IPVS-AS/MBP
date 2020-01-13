074bd4963e525a12427118a45ee9edcf900dd1c1
Jan Schneider <st117301@stud.uni-stuttgart.de>
2020-01-13 23:53:58 +0100

    Enabled sharing of rules, rule triggers and rule actions

--- src/main/java/org/citopt/connde/web/rest/RestUserApprovalController.java
+++ src/main/java/org/citopt/connde/web/rest/RestUserApprovalController.java
@@ -7,6 +7,7 @@ import org.citopt.connde.domain.component.Sensor;
 import org.citopt.connde.domain.device.Device;
 import org.citopt.connde.domain.monitoring.MonitoringAdapter;
 import org.citopt.connde.domain.monitoring.MonitoringComponent;
+import org.citopt.connde.domain.rules.Rule;
 import org.citopt.connde.domain.user.User;
 import org.citopt.connde.domain.user_entity.UserEntity;
 import org.citopt.connde.repository.*;
@@ -14,6 +15,7 @@ import org.citopt.connde.repository.projection.ComponentExcerpt;
 import org.citopt.connde.service.UserEntityService;
 import org.citopt.connde.service.UserService;
 import org.citopt.connde.service.deploy.SSHDeployer;
+import org.citopt.connde.service.rules.RuleEngine;
 import org.citopt.connde.web.rest.helper.MonitoringHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * REST Controller for approval and disapproval of users for user entities.
@@ -54,10 +57,22 @@ public class RestUserApprovalController {
     private SensorRepository sensorRepository;
 
     @Autowired
-    private MonitoringHelper monitoringHelper;
+    private RuleTriggerRepository ruleTriggerRepository;
+
+    @Autowired
+    private RuleActionRepository ruleActionRepository;
+
+    @Autowired
+    private RuleRepository ruleRepository;
 
     @Autowired
     private SSHDeployer sshDeployer;
+
+    @Autowired
+    private RuleEngine ruleEngine;
+
+    @Autowired
+    private MonitoringHelper monitoringHelper;
 
     @PostMapping("/adapters/{adapterId}/approve")
     @ApiOperation(value = "Approves an user for an adapter entity", produces = "application/hal+json")
@@ -125,7 +140,6 @@ public class RestUserApprovalController {
     public ResponseEntity<Void> approveForDevice(@PathVariable @ApiParam(value = "ID of the device to approve an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String deviceId, @RequestBody @ApiParam(value = "Name of the user to approve", example = "johndoe", required = true) String username) {
         return approveUserEntity(deviceId, username, deviceRepository);
     }
-
 
     @PostMapping("/devices/{deviceId}/disapprove")
     @ApiOperation(value = "Disapproves an user for a device entity", produces = "application/hal+json")
@@ -250,7 +264,6 @@ public class RestUserApprovalController {
         return approveUserEntity(actuatorId, username, actuatorRepository);
     }
 
-
     @PostMapping("/actuators/{actuatorId}/disapprove")
     @ApiOperation(value = "Disapproves an user for an actuator entity", produces = "application/hal+json")
     @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User cannot be disapproved for this actuator"), @ApiResponse(code = 403, message = "Not authorized to disapprove an user for this actuator"), @ApiResponse(code = 404, message = "Actuator or user not found")})
@@ -265,13 +278,103 @@ public class RestUserApprovalController {
         return approveUserEntity(sensorId, username, sensorRepository);
     }
 
-
     @PostMapping("/sensors/{sensorId}/disapprove")
     @ApiOperation(value = "Disapproves an user for a sensor entity", produces = "application/hal+json")
     @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User cannot be disapproved for this sensor"), @ApiResponse(code = 403, message = "Not authorized to disapprove an user for this sensor"), @ApiResponse(code = 404, message = "Sensor or user not found")})
-    public ResponseEntity<Void> disapproveForSensor(@PathVariable @ApiParam(value = "ID of the sensor to disapprove an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String sensorId, @RequestBody @ApiParam(value = "Name of the sensor to disapprove", example = "johndoe", required = true) String username) {
+    public ResponseEntity<Void> disapproveForSensor(@PathVariable @ApiParam(value = "ID of the sensor to disapprove an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String sensorId, @RequestBody @ApiParam(value = "Name of the user to disapprove", example = "johndoe", required = true) String username) {
         return disapproveUserEntity(sensorId, username, sensorRepository);
     }
+
+    @PostMapping("/rule-triggers/{triggerId}/approve")
+    @ApiOperation(value = "Approves an user for a rule trigger entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User is already approved for this rule trigger"), @ApiResponse(code = 403, message = "Not authorized to approve an user for this rule trigger"), @ApiResponse(code = 404, message = "Rule trigger or user not found")})
+    public ResponseEntity<Void> approveForRuleTrigger(@PathVariable @ApiParam(value = "ID of the rule trigger to approve an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String triggerId, @RequestBody @ApiParam(value = "Name of the user to approve", example = "johndoe", required = true) String username) {
+        return approveUserEntity(triggerId, username, ruleTriggerRepository);
+    }
+
+    @PostMapping("/rule-triggers/{triggerId}/disapprove")
+    @ApiOperation(value = "Disapproves an user for a rule trigger entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User cannot be disapproved for this rule trigger"), @ApiResponse(code = 403, message = "Not authorized to disapprove an user for this rule trigger"), @ApiResponse(code = 404, message = "Rule trigger or user not found")})
+    public ResponseEntity<Void> disapproveForRuleTrigger(@PathVariable @ApiParam(value = "ID of the rule trigger to disapprove an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String triggerId, @RequestBody @ApiParam(value = "Name of the user to disapprove", example = "johndoe", required = true) String username) {
+        //TODO clean up and extract common helper method for this
+
+        //Disapprove user for the entity
+        ResponseEntity<Void> disapprovalResult = disapproveUserEntity(triggerId, username, ruleTriggerRepository);
+
+        //Check result
+        if (!disapprovalResult.getStatusCode().equals(HttpStatus.OK)) {
+            return disapprovalResult;
+        }
+
+        //Get all possibly affected rules
+        List<Rule> affectedRules = ruleRepository.findAllByTriggerId(triggerId);
+
+        //Check rules
+        for (Rule affectedRule : affectedRules) {
+            //Check if rule is owned by the affected user
+            if (username.equals(affectedRule.getOwnerName())) {
+                //Disable and delete rule
+                ruleEngine.disableRule(affectedRule);
+                ruleRepository.delete(affectedRule);
+            }
+        }
+
+        return disapprovalResult;
+    }
+
+    @PostMapping("/rule-actions/{actionId}/approve")
+    @ApiOperation(value = "Approves an user for a rule action entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User is already approved for this rule action"), @ApiResponse(code = 403, message = "Not authorized to approve an user for this rule action"), @ApiResponse(code = 404, message = "Rule action or user not found")})
+    public ResponseEntity<Void> approveForRuleAction(@PathVariable @ApiParam(value = "ID of the rule action to approve an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String actionId, @RequestBody @ApiParam(value = "Name of the user to approve", example = "johndoe", required = true) String username) {
+        return approveUserEntity(actionId, username, ruleActionRepository);
+    }
+
+    @PostMapping("/rule-actions/{actionId}/disapprove")
+    @ApiOperation(value = "Disapproves an user for a rule action entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User cannot be disapproved for this rule action"), @ApiResponse(code = 403, message = "Not authorized to disapprove an user for this rule action"), @ApiResponse(code = 404, message = "Rule action or user not found")})
+    public ResponseEntity<Void> disapproveForRuleAction(@PathVariable @ApiParam(value = "ID of the rule action to disapprove an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String actionId, @RequestBody @ApiParam(value = "Name of the user to disapprove", example = "johndoe", required = true) String username) {
+        //Disapprove user for the entity
+        ResponseEntity<Void> disapprovalResult = disapproveUserEntity(actionId, username, ruleActionRepository);
+
+        //Check result
+        if (!disapprovalResult.getStatusCode().equals(HttpStatus.OK)) {
+            return disapprovalResult;
+        }
+
+        //Get all possibly affected rules
+        List<Rule> affectedRules = ruleRepository.findAll()
+                .stream() //Consider each rule in repository
+                //Filter for rules that contain an action with matching id
+                .filter(rule -> rule.getActions().stream().anyMatch(action -> action.getId().equals(actionId)))
+                .collect(Collectors.toList());
+
+        //Check rules
+        for (Rule affectedRule : affectedRules) {
+            //Check if rule is owned by the affected user
+            if (username.equals(affectedRule.getOwnerName())) {
+                //Disable and delete rule
+                ruleEngine.disableRule(affectedRule);
+                ruleRepository.delete(affectedRule);
+            }
+        }
+
+        return disapprovalResult;
+    }
+
+    @PostMapping("/rules/{ruleId}/approve")
+    @ApiOperation(value = "Approves an user for a rule entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User is already approved for this rule"), @ApiResponse(code = 403, message = "Not authorized to approve an user for this rule"), @ApiResponse(code = 404, message = "Rule or user not found")})
+    public ResponseEntity<Void> approveForRule(@PathVariable @ApiParam(value = "ID of the rule to approve an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String ruleId, @RequestBody @ApiParam(value = "Name of the user to approve", example = "johndoe", required = true) String username) {
+        return approveUserEntity(ruleId, username, ruleRepository);
+    }
+
+    @PostMapping("/rules/{ruleId}/disapprove")
+    @ApiOperation(value = "Disapproves an user for a rule entity", produces = "application/hal+json")
+    @ApiResponses({@ApiResponse(code = 200, message = "Success"), @ApiResponse(code = 400, message = "User cannot be disapproved for this rule"), @ApiResponse(code = 403, message = "Not authorized to disapprove an user for this rule"), @ApiResponse(code = 404, message = "Rule or user not found")})
+    public ResponseEntity<Void> disapproveForRule(@PathVariable @ApiParam(value = "ID of the rule to disapprove an user for", example = "5c97dc2583aeb6078c5ab672", required = true) String ruleId, @RequestBody @ApiParam(value = "Name of the user to disapprove", example = "johndoe", required = true) String username) {
+        return disapproveUserEntity(ruleId, username, ruleRepository);
+    }
+
 
     /**
      * Tries to approve an user for an user entity from a repository and generates a corresponding response for
