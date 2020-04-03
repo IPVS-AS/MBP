940af3b8dfddd3d9be6eabd3a080289f279bae87
lauracorssac <lauracorssac@gmail.com>
2020-04-03 12:22:12 +0200

    script to automate program execution after code changes

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#########################################################################################################################
+# This script automates the process of executing the program after changes in code.
+
+# It only works on Linux
+# To run it, make sure you are in the project's main folder and 
+# $ sh ./update.sh
+
+#########################################################################################################################
+
+# Creates a new MBP.war package in project's taget folder
+echo "Building .war file..."   
+sudo mvn clean install
+
+# Deploys .war file by moving it to tomcat's webapps folder
+echo "Deploying .war file..." 
+sudo mv target/MBP-0.1.war /var/lib/tomcat8/webapps/MBP.war
+
+# Restart tomcat8
+echo "Restarting tomcat..." 
+sudo service tomcat8 restart
+
+echo "Update finished. Good luck! ;)" 
+# you now can find your modified application in localhost:8080/MBP
+# I hope it works! 
+# If if doesn't, you can always change your code and re-run this script :)
+
+
+
