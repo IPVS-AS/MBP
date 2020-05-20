d498922638c56ec3784e1540266ad372aed9d6aa
Jan Schneider <st117301@stud.uni-stuttgart.de>
2020-05-20 16:52:47 +0200

    Fixed merging issue

--- src/main/resources/static/js/controllers/home/HomeController.js
+++ src/main/resources/static/js/controllers/home/HomeController.js
@@ -1,12 +1,23 @@
 /* global app */
 
 app.controller('HomeController',
-        ['$scope', '$timeout', 'CrudService', 'ComponentService', 'DeviceService', 'countEnvModels', 'countActuators', 'countSensors', 'countDevices', 'countAdapters', 'countMonitoringAdapters', 'countTests',
-            function ($scope, $timeout, CrudService, ComponentService, DeviceService, countEnvModels, countActuators, countSensors, countDevices, countAdapters, countMonitoringAdapters, countTests) {
-                vm.loader = {};
+    ['$scope', '$timeout', 'CrudService', 'ComponentService', 'DeviceService', 'countEnvModels',
+        'countActuators', 'countSensors', 'countDevices', 'countAdapters', 'countMonitoringAdapters', 'countTests',
+        function ($scope, $timeout, CrudService, ComponentService, DeviceService, countEnvModels,
+                  countActuators, countSensors, countDevices, countAdapters, countMonitoringAdapters, countTests) {
 
-                //Expose
-                    countEnvModels: countEnvModels,
-                    countMonitoringAdapters: countMonitoringAdapters,
-                    countTests: countTests
+            let vm = this;
+            vm.loader = {};
+
+            //Expose
+            angular.extend(vm, {
+                countEnvModels: countEnvModels,
+                countActuators: countActuators,
+                countSensors: countSensors,
+                countDevices: countDevices,
+                countAdapters: countAdapters,
+                countMonitoringAdapters: countMonitoringAdapters,
+                countTests: countTests
+            });
+        }]);
 
