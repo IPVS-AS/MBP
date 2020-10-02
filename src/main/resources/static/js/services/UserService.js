10826ef1b9e047c4a5d74d1dcc70850b9cb8003a
Jan Schneider <st117301@stud.uni-stuttgart.de>
2020-10-02 11:46:42 +0200

    Updated attribute stuff

--- src/main/resources/static/js/services/UserService.js
+++ src/main/resources/static/js/services/UserService.js
@@ -25,53 +25,37 @@
          * [Public]
          */
         function getUserAttributes() {
-            return "firstName=" + $rootScope.globals.currentUser.userData.firstName + ";;"
-                + "lastName=" + $rootScope.globals.currentUser.userData.lastName + ";;"
-                + "username=" + $rootScope.globals.currentUser.username;
+            return "requesting-entity-firstname=" + $rootScope.globals.currentUser.userData.firstName + ";;"
+                + "requesting-entity-lastname=" + $rootScope.globals.currentUser.userData.lastName + ";;"
+                + "requesting-entity-username=" + $rootScope.globals.currentUser.username;
         }
 
         function Authenticate(user) {
-            return $http.post(ENDPOINT_URI + '/authenticate', user).then(handleSuccess, handleError);
+            return $http.post(ENDPOINT_URI + '/authenticate', user);
         }
 
         function Logout() {
-            return $http.get(BASE_URI + 'logout').then(handleSuccess, handleError);
+            return $http.get(BASE_URI + 'logout');
         }
 
         function GetAll() {
-            return $http.get(ENDPOINT_URI + '/users').then(handleSuccess, handleError);
+            return $http.get(ENDPOINT_URI + '/users');
         }
 
         function GetByUsername(username) {
-            return $http.get(ENDPOINT_URI + '/users/' + username).then(handleSuccess, handleError);
+            return $http.get(ENDPOINT_URI + '/users/' + username);
         }
 
         function Create(user) {
-            return $http.post(ENDPOINT_URI + '/users', user).then(handleSuccess, handleError);
+            return $http.post(ENDPOINT_URI + '/users', user);
         }
 
         function Update(user) {
-            return $http.put(ENDPOINT_URI + '/users/', user).then(handleSuccess, handleError);
+            return $http.put(ENDPOINT_URI + '/users/', user);
         }
 
         function Delete(username) {
-            return $http.delete(ENDPOINT_URI + '/users/' + username).then(handleSuccess, handleError);
-        }
-
-        function handleSuccess(res) {
-            return {
-                success: true,
-                message: res.headers('X-MBP-alert'),
-                data: res.data
-            };
-        }
-
-        function handleError(res) {
-            return {
-                success: false,
-                message: res.headers('X-MBP-error'),
-                status: res.status
-            };
+            return $http.delete(ENDPOINT_URI + '/users/' + username);
         }
     }
 
