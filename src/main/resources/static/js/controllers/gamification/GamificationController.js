e9874bde804c281b4cbbf87a04df2267f74944fe
tatianegava <tatinha.gava@gmail.com>
2021-01-23 23:36:37 +0100

    Changes in web interface

--- src/main/resources/static/js/controllers/gamification/GamificationController.js
+++ src/main/resources/static/js/controllers/gamification/GamificationController.js
@@ -10,6 +10,7 @@
   function GamificationController($location, $scope, GamificationUserService, FlashService) {
     var vm = this;
     vm.activities = [];
+    vm.levels = [];
 	vm.GFUser = {};
 	vm.storeItemList= [];
 	vm.selectedActivity = {};
@@ -81,12 +82,43 @@
           if (response.success) {
           	  //FlashService.Success(JSON.stringify(response));
           	  vm.activities = response.data.quests;
+          	  
+          	  vm.levels = new Array();
+          	  var previousLevel = "";
+          	  var previousLevelIdxStart = 0;
+              for (var i = 0; i < vm.activities.length; i++) {
+	              if (vm.activities[i].level != previousLevel) {
+	           		  vm.activities[i].firstOfLevel = true;
+	           		  vm.levels.push(vm.activities[i].level);
+	           		  /*if (i > 0) { 
+	           		  	vm.levelCount[vm.activities[i-1].level] = i - previousLevelIdxStart; 
+	           		  }*/
+	           		  previousLevel = vm.activities[i].level;
+	           		  previousLevelIdxStart = i;  
+	              }
+	              
+	              //Last item - count for last level
+	              /*if (i == vm.activities.length-1) {
+	                  vm.levelCount[vm.activities[i].level] = i - previousLevelIdxStart + 1;
+	              }*/
+              }
           } else {
               FlashService.Error(response.message);
           }
         }); 
       
       vm.dataLoading = false;
+    };
+    
+    function countLevelActivities(p_level) {
+    	var count = 0;
+    	for (var i = 0; i <= vm.activities.length; i++) {
+          if (vm.activities[i].level == p_level) {
+       		  count++;
+          }
+      	}
+      	
+      	return count;
     };
     
     function setGamificationContentProgress() {
@@ -103,13 +135,31 @@
     function loadStoreData() {
       vm.dataLoading = true;
       
-      var item1 = {id:1, name:'5€ Greenpeace', price:1000};
-      var item2 = {id:2, name:'Planted Tree', price:500};
-      var item3 = {id:3, name:'Reusable Bag', price:500};
+      var item1 = {id:1, name:'5€ to Greenpeace', price:4300};
+      var item2 = {id:2, name:'Surprise', price:4300};
+      var item3 = {id:3, name:'1 Green Chocolate', price:2500};
+      var item4 = {id:4, name:'1€ to Project Tres', price:1000};
+      var item5 = {id:5, name:'1€ to Greenpeace', price:1000};
+      var item6 = {id:6, name:'1 Fruit/Vegetable Bag', price:1000};
+      var item7 = {id:7, name:'2 Eco Trash Bags', price:500};
+      var item8 = {id:8, name:'1 Bio Vegetable', price:500};
+      var item9 = {id:9, name:'1 Eco Trash Bag', price:300};
+      var item10 = {id:10, name:'1 Bio Apple', price:300};
+      var item11 = {id:11, name:'1 Coffee', price:100};
+      var item12 = {id:12, name:'1 Tea', price:100};
       vm.storeItemList = new Array();
       vm.storeItemList.push(item1);
       vm.storeItemList.push(item2);
       vm.storeItemList.push(item3);
+      vm.storeItemList.push(item4);
+      vm.storeItemList.push(item5);
+      vm.storeItemList.push(item6);
+      vm.storeItemList.push(item7);
+      vm.storeItemList.push(item8);
+      vm.storeItemList.push(item9);
+      vm.storeItemList.push(item10);
+      vm.storeItemList.push(item11);
+      vm.storeItemList.push(item12);
       
       vm.dataLoading = false;
     };
