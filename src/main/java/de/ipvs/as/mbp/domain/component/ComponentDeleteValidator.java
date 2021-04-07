2f6fbfff1a1dc8fbc0ce315b9b44f67be1f5b142
Jan Schneider <st117301@stud.uni-stuttgart.de>
2021-04-08 00:15:42 +0200

    Added demonstration mode to settings

--- src/main/java/de/ipvs/as/mbp/domain/component/ComponentDeleteValidator.java
+++ src/main/java/de/ipvs/as/mbp/domain/component/ComponentDeleteValidator.java
@@ -1,30 +1,31 @@
 package de.ipvs.as.mbp.domain.component;
 
-import de.ipvs.as.mbp.domain.component.Component;
 import de.ipvs.as.mbp.error.MBPException;
-import de.ipvs.as.mbp.service.deploy.SSHDeployer;
+import de.ipvs.as.mbp.service.deployment.DeployerDispatcher;
+import de.ipvs.as.mbp.service.deployment.IDeployer;
 import de.ipvs.as.mbp.service.validation.IDeleteValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
 public class ComponentDeleteValidator implements IDeleteValidator<Component> {
 
     @Autowired
-    private SSHDeployer sshDeployer;
+    private DeployerDispatcher deployerDispatcher;
 
     @Override
     public void validateDeletable(Component component) {
+        //Find suitable deployer component
+        IDeployer deployer = deployerDispatcher.getDeployer();
+
         try {
             //Try to determine deployment status
-            if (sshDeployer.isComponentDeployed(component)) {
+            if (deployer.isComponentDeployed(component)) {
                 //Component is deployed, notify the user
                 throw new MBPException(HttpStatus.CONFLICT, "Component '" + component.getName() + "' cannot be deleted since it is still deployed.");
             }
-        } catch (IOException ignored) {
+        } catch (Exception ignored) {
             //Could not determine deployment status, delete anyway
         }
     }
