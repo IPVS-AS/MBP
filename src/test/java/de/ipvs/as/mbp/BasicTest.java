70e134973ba8e3222da4246a49331a7d9c6aadb5
Christian Müller <dev@c-mueller.xyz>
2021-05-28 11:22:25 +0200

    Enable MockMVC

--- src/test/java/de/ipvs/as/mbp/BasicTest.java
+++ src/test/java/de/ipvs/as/mbp/BasicTest.java
@@ -2,16 +2,24 @@ package de.ipvs.as.mbp;
 
 import de.ipvs.as.mbp.util.BaseIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class BasicTest extends BaseIntegrationTest {
-    @Test
-    void name() {
 
+    @Autowired
+    public MockMvc mockMvc;
+
+    @Test
+    void name() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/de/ipvs/as/mbp/util/BaseIntegrationTest.java
+++ src/test/java/de/ipvs/as/mbp/util/BaseIntegrationTest.java
@@ -2,6 +2,9 @@ package de.ipvs.as.mbp.util;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -13,12 +16,15 @@ import org.testcontainers.utility.DockerImageName;
 @Testcontainers(disabledWithoutDocker = true)
 @ContextConfiguration(classes = {IntegrationTestConfiguration.class})
 @SpringBootTest()
+@AutoConfigureMockMvc
+@AutoConfigureWebMvc
 @ExtendWith(MockitoExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public abstract class BaseIntegrationTest {
 
     @Container
     public GenericContainer mongoDbContainer = new GenericContainer("mongo:latest")
-            .withExposedPorts(27017);
+            .withExposedPorts(27017)
+            .withEnv("MONGO_INITDB_DATABASE", "mbp");
 
 }
