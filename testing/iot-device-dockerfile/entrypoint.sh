7f84dbe67c6248e3935e712658fcc83c8af752cf
Christian Müller <dev@c-mueller.xyz>
2021-06-04 22:34:56 +0200

    Add Dockerfile for Mock IoT Device

---
+++ testing/iot-device-dockerfile/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ssh-keygen -P "" -t dsa -f /etc/ssh/ssh_host_dsa_key
+ssh-keygen -P "" -t rsa -f /etc/ssh/ssh_host_rsa_key
+ssh-keygen -P "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
+/usr/sbin/sshd -D
