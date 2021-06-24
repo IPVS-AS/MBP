d53acc41f0f417912d8aef980922305226cc7701
Christian Müller <dev@c-mueller.xyz>
2021-06-24 13:48:43 +0200

    Add Basic device test

--- src/test/java/de/ipvs/as/mbp/BasicDeviceTest.java
+++ src/test/java/de/ipvs/as/mbp/BasicDeviceTest.java
@@ -1,14 +1,22 @@
 package de.ipvs.as.mbp;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
 import javax.servlet.http.Cookie;
 
+import com.jcabi.ssh.Shell;
+import de.ipvs.as.mbp.domain.device.Device;
 import de.ipvs.as.mbp.domain.device.DeviceDTO;
 import de.ipvs.as.mbp.util.BaseDeviceTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class BasicDeviceTest extends BaseDeviceTest {
@@ -31,5 +39,28 @@ public class BasicDeviceTest extends BaseDeviceTest {
                 .characterEncoding("utf-8"))
                 .andDo(print())
                 .andExpect(status().isOk()).andReturn();
+
+        Device deviceObj = objectMapper.readValue(result.getResponse().getContentAsString(), Device.class);
+
+        result = mockMvc.perform(get(RestConfiguration.BASE_PATH + "/devices/" + deviceObj.getId() + "/state/")
+                .header("X-MBP-Access-Request", "requesting-entity-firstname=admin;;requesting-entity-lastname=admin;;requesting-entity-username=admin")
+                .cookie(sessionCookie))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("SSH_AVAILABLE"))
+                .andDo(print())
+                .andReturn();
+
+        Shell shell = device.openSshShell();
+        ByteArrayOutputStream stdout = new ByteArrayOutputStream();
+        ByteArrayOutputStream stderr = new ByteArrayOutputStream();
+
+        shell.exec("sudo cat /var/log/auth.log", new ByteArrayInputStream(new byte[0]), stdout, stderr);
+
+        String stdoutString = stdout.toString();
+
+        System.out.println(stdoutString);
+
+        // Check that the testing command has been called in the proper context
+        assertThat(stdoutString).contains("mbp : PWD=/home/mbp ; USER=root ; COMMAND=/usr/bin/test 5 -gt 2");
     }
 }
--- testing/iot-device-dockerfile/entrypoint.sh
+++ testing/iot-device-dockerfile/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rsyslogd
 ssh-keygen -P "" -t dsa -f /etc/ssh/ssh_host_dsa_key
 ssh-keygen -P "" -t rsa -f /etc/ssh/ssh_host_rsa_key
 ssh-keygen -P "" -t ed25519 -f /etc/ssh/ssh_host_ed25519_key
