e5ce8d907f74317cf2926e8b1ee2a494f4e79704
Jan Schneider <st117301@stud.uni-stuttgart.de>
2021-08-04 01:01:21 +0200

    Try to workaround the Reflections issue

---
+++ src/main/java/de/ipvs/as/mbp/domain/discovery/collections/revision/operations/ReflectionsFixEnum.java
@@ -0,0 +1,8 @@
+package de.ipvs.as.mbp.domain.discovery.collections.revision.operations;
+
+/**
+ * Useless enum as workaround for an issue with the Reflection library:
+ * https://github.com/ronmamo/reflections/issues/273
+ */
+public enum ReflectionsFixEnum {
+}
