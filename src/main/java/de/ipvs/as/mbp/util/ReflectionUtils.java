11d8996dce60295a2be8831a6cd125e6fd41d6d9
Jan Schneider <st117301@stud.uni-stuttgart.de>
2021-08-04 05:52:29 +0200

    Next try for a workaround

--- src/main/java/de/ipvs/as/mbp/util/ReflectionUtils.java
+++ src/main/java/de/ipvs/as/mbp/util/ReflectionUtils.java
@@ -1,5 +1,6 @@
 package de.ipvs.as.mbp.util;
 
+import de.ipvs.as.mbp.domain.discovery.collections.revision.operations.UpsertOperation;
 import org.reflections.Reflections;
 
 import java.util.Set;
@@ -7,7 +8,7 @@ import java.util.Set;
 public class ReflectionUtils {
 
     private static final String DOMAIN_PACKAGE = "de.ipvs.as.mbp.domain";
-    private static final Reflections REFLECTIONS = new Reflections(DOMAIN_PACKAGE);
+    private static final Reflections REFLECTIONS = new Reflections(DOMAIN_PACKAGE, UpsertOperation.class.getClassLoader());
 
     public static <T> Set<Class<? extends T>> getSubTypes(Class<T> type) {
         return REFLECTIONS.getSubTypesOf(type);
