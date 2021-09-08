21cfe2971953733bf6e302ddd5c8cc88558ac7eb
schneijan <st117301@stud.uni-stuttgart.de>
2021-09-08 02:34:07 +0200

    Added application class again

---
+++ src/main/java/de/ipvs/as/mbp/HalConfiguration.java
@@ -0,0 +1,7 @@
+package de.ipvs.as.mbp;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HalConfiguration extends org.springframework.hateoas.mediatype.hal.HalConfiguration {
+}
---
+++ src/main/java/de/ipvs/as/mbp/MBPApplication.java
@@ -0,0 +1,14 @@
+/*
+package de.ipvs.as.mbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class MBPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MBPApplication.class, args);
+    }
+}*/
