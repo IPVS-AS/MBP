aaff597d24c4dc3cfe1fcdf5c4d33569779895cd
Christian Müller <dev@c-mueller.xyz>
2021-09-19 13:01:56 +0200

    Fix test

--- src/test/resources/scripts/mbp_client/start.sh
+++ src/test/resources/scripts/mbp_client/start.sh
@@ -2,8 +2,9 @@
 
 DIR=`dirname $0`
 cd $DIR
-sed -i '2s/.*/brokerHost=10.133.1.65/' mbp.properties
+#sed -i '2s/.*/brokerHost=10.133.1.65/' mbp.properties
 #sed -i '2s/.*/brokerHost=host.docker.internal/' mbp.properties
+#sed -i '2s/.*/brokerHost=172.17.0.1/' mbp.properties
 
 # ---------Attention----------------
 # If you rename the main python file of the operator, update the content of the entry-file-name accordingly
